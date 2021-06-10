002a3c598b05d8612762913f16799192fef4fe47
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 16:28:36 -0500

    Added boiler plate for application.

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,12 @@ import {Router, Route, Switch} from "react-router-dom";
 import {Container} from "reactstrap";
 
 //import components
+import Header from "./components/Header/Header";
 import NavBar from "./components/Nav/NavBar";
-
-
+import Project from "./components/Project/Project";
+import Profile from "./components/Profile/Profile";
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
 //import styles
 
 
@@ -13,13 +16,16 @@ import NavBar from "./components/Nav/NavBar";
 const App=()=>{
   return(
     <Router>
+      <Header/>
       <NavBar/>
       <Container>
         <Switch>
-          <Route path="/" exact component={Header}/>
-          
+          <Route path="/" exact component={Profile}/>
+          <Route path="/project" exact component={Project}/>
+          <Route path="/contact" exact component={Contact}/>
         </Switch>
       </Container>
+      <Footer/>
     </Router>
   )
 }
