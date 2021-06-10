dee312821c6b9a3bc25fba08f522abe9adfd6e00
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 15:48:24 -0500

    Added footer to page.

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+
+const Footer = () => (
+  <footer id="footer" className="text-center">
+    <p>
+     Npaus Portfolio created by Bao Xiong
+    </p>
+  </footer>
+);
+
+export default Footer;
