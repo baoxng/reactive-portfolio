7de248e600ada4d99c8d13c8ceb0acb6595c2d26
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 23:50:37 -0500

    Added build.

---
+++ client/build/precache-manifest.2e67873d48ad61669ae79ae7e2fd7e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d785968e713f4ed0bda323e0d7620b79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4dbbbf7c2493550eb8a",
+    "url": "/static/js/2.02f8f9ba.chunk.js"
+  },
+  {
+    "revision": "aa45008416387dc1a57e8c95c452f9d2",
+    "url": "/static/js/2.02f8f9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11bbdf30a245bcebf9d0",
+    "url": "/static/js/main.5f4bcd2a.chunk.js"
+  },
+  {
+    "revision": "37909bd5c20e7f31f663",
+    "url": "/static/js/runtime-main.2a6f9376.js"
+  },
+  {
+    "revision": "6f4d9ad51b3b924fb63c86fd398b980d",
+    "url": "/static/media/BaoResume.6f4d9ad5.pdf"
+  }
+]);
