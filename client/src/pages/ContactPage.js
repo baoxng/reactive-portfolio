36992830df53c82fee4821859450680187853bfc
Bao Xiong <bao.xiong2011@gmail.com>
2021-06-10 22:45:28 -0500

    Added to contact page.

--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -6,8 +6,15 @@ function ContactPage() {
   return (
     <Container fluid>
       <Row>
-      <Col size="md-12 sm-12">
+        <Col size="md-6 sm-12">
           <Contact />
+        </Col>
+        <Col size="md-6 sm-12">
+        <p>
+          <h1>Connect with me:</h1>
+        </p>
+        <h3><a href="https://github.com/baoxng"><span>GitHub</span><i class="fab fa-github-square"></i></a></h3>
+        <h3><a href="https://www.linkedin.com/in/bao-xiong/"><span>LinkedIn</span><i class="fab fa-linkedin"></i></a></h3>
         </Col>
       </Row>
     </Container>
